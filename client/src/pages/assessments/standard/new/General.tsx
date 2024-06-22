0f5b14a2f129abfce29e0d1507bd37c2fcf0c51f
annuraggg <anuragsawant@duck.com>
2024-06-22 14:58:39 +0530

    chore: Fix typo in General.tsx file

--- client/src/pages/assessments/standard/new/General.tsx
+++ client/src/pages/assessments/standard/new/General.tsx
@@ -43,7 +43,7 @@ const General = ({
 }) => {
   return (
     <div className="flex gap-10 h-full">
-      <motion.div
+     <motion.div
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3 }}
