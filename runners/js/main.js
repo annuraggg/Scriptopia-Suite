dc80aeba7f1a6e804df210b78a6b274a9648f74b
= <=>
2024-06-30 22:13:04 +0530

    Added Code Runners for JS and Python

---
+++ runners/js/main.js
@@ -0,0 +1,102 @@
+import { performance } from "perf_hooks";
+
+export const handler = async (event) => {
+  try {
+    const {
+      functionName,
+      functionArgs,
+      functionBody,
+      functionReturn,
+      testCases,
+    } = event;
+
+    const fnScript = createFunctionScript(
+      functionName,
+      functionArgs,
+      functionBody,
+      functionReturn
+    );
+
+    const { results, avgTime, avgMemory } = runTestCases(
+      functionName,
+      fnScript,
+      testCases
+    );
+
+    const failedCase = results.find((result) => !result.passed);
+    const status = failedCase ? "FAILED" : "PASSED";
+    const failedCaseNo = failedCase ? failedCase.caseNo : -1;
+
+    return {
+      STATUS: status,
+      failedCaseNo: failedCaseNo,
+      avgTime: avgTime,
+      avgMemory: avgMemory,
+      results: results,
+    };
+  } catch (error) {
+    console.error("Error executing function:", error);
+    return {
+      STATUS: "ERROR",
+      message: error.message,
+    };
+  }
+};
+
+const createFunctionScript = (
+  functionName,
+  functionArgs,
+  functionBody,
+  functionReturn
+) => {
+  const args = functionArgs.map((arg) => arg.name);
+
+  const fn = `
+    function ${functionName}(${args.join(", ")}) {
+        ${functionBody}
+    }
+    `;
+
+  return fn;
+};
+
+const runTestCases = (functionName, fnScript, testCases) => {
+  const results = [];
+  let totalTime = 0;
+  let totalMemory = 0;
+
+  testCases.forEach((testCase, index) => {
+    const { time, memory, passed, output } = executeFn(
+      functionName,
+      fnScript,
+      testCase
+    );
+
+    results.push({ caseNo: index + 1, time, memory, passed, output });
+    totalTime += time;
+    totalMemory += memory;
+  });
+
+  const avgTime = totalTime / testCases.length;
+  const avgMemory = totalMemory / testCases.length;
+
+  return { results, avgTime, avgMemory };
+};
+
+const executeFn = (functionName, fnScript, testCase) => {
+  const { input, output } = testCase;
+
+  const start = performance.now();
+  const initialMemory = process.memoryUsage().heapUsed;
+
+  const result = new Function(`return (${fnScript});`)()(...input);
+
+  const end = performance.now();
+  const finalMemory = process.memoryUsage().heapUsed;
+
+  const time = end - start;
+  const memory = finalMemory - initialMemory;
+  const passed = result === output;
+
+  return { time, memory, passed, output: result };
+};
