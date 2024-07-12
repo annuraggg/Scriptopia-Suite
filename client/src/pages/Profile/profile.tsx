45b76d0f50cee239410055f47c9206545362083a
Spy156 <d.man1562001@gmail.com>
2024-07-13 00:07:25 +0530

    assessment and profile pages design completed

--- client/src/pages/Profile/profile.tsx
+++ client/src/pages/Profile/profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { motion } from "framer-motion";
 import LeftPane from "./LeftPane";
 import RightPane from "./RightPane";
--- client/src/pages/assessments/dashboard/ViewAssessment/ViewUserAssessment.tsx
+++ client/src/pages/assessments/dashboard/ViewAssessment/ViewUserAssessment.tsx
@@ -1,23 +1,4 @@
-import React, { Key, useState }  from 'react'
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    Input,
-    Table,
-    TableHeader,
-    TableColumn,
-    TableBody,
-    TableRow,
-    TableCell,
-    Button,
-    Tabs,
-    Tab,
-    Dropdown,
-    DropdownTrigger,
-    DropdownMenu,
-    DropdownItem,
-} from "@nextui-org/react";
+
 
 const ViewUserAssessment = () => {
   return (
