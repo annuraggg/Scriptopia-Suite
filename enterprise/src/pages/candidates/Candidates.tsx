9757644e1a17549b3384e1a05c6c32dd5204a0b0
= <anuragsawant@duck.com>
2024-07-27 10:37:27 +0530

    refactor: Update Candidates page to include breadcrumbs and organization information

--- enterprise/src/pages/candidates/Candidates.tsx
+++ enterprise/src/pages/candidates/Candidates.tsx
@@ -1,23 +1,39 @@
-import { DataTable } from "@/components/DataTable";
+import { DataTable } from "./DataTable";
+import { RootState } from "@/@types/reducer";
+import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/breadcrumbs";
+
+import { useSelector } from "react-redux";
 
 const Candidates = () => {
-  const tableData = [];
+  const org = useSelector((state: RootState) => state.organization);
 
+  const tableData = [];
+  const statuses = ["Qualified", "Disqualified", "Hired"];
   for (let i = 1; i <= 50; i++) {
     const record = {
       name: `Candidate ${i}`,
       email: `candidate${i}@example.com`,
       received: `${Math.floor(Math.random() * 28) + 1}th July`,
-      match: `${Math.floor(Math.random() * 100)}%`,
+      status: statuses[Math.floor(Math.random() * 3)],
     };
 
     tableData.push(record);
   }
 
   return (
-    <div className="p-5">
-      <DataTable data={tableData} />
-    </div>
+    <>
+      <div className="mt-5 ml-5">
+        <Breadcrumbs>
+          <BreadcrumbItem href={"/" + org._id}>Organization</BreadcrumbItem>
+          <BreadcrumbItem href={"/" + org._id + "/candidates"}>
+            Candidates
+          </BreadcrumbItem>
+        </Breadcrumbs>
+      </div>
+      <div className="p-5">
+        <DataTable data={tableData} />
+      </div>
+    </>
   );
 };
 
