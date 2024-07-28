90eaf0d8a8d6e7fca35f8d86088cffa961d085ef
= <anuragsawant@duck.com>
2024-07-28 14:09:05 +0530

    refactor: Remove unused Sentry dependencies and configuration
    The `package.json` file in the server directory has been modified to remove the `@sentry/cli`, `@sentry/node`, and `@sentry/profiling-node` dependencies. Additionally, the `instrument.ts` file has been deleted, which contained the configuration for Sentry. These changes remove unused Sentry dependencies and configuration from the project.

--- server/src/config/init.ts
+++ server/src/config/init.ts
@@ -1,10 +1,8 @@
 import "dotenv/config";
-import "./instrument";
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { cors } from "hono/cors";
 import { clerkMiddleware } from "@hono/clerk-auth";
-import { sentry } from "@hono/sentry";
 
 import performanceMiddleware from "../middlewares/performanceMiddleware";
 import authMiddleware from "../middlewares/authMiddleware";
@@ -26,13 +24,7 @@ import userRoute from "../routes/userRoute";
 const app = new Hono();
 
 // @ts-expect-error - Types Not Available
-app.use(clerkMiddleware()); // @ts-expect-error - Types Not Available
-app.use(
-  "*",
-  sentry({
-    dsn: process.env.SENTRY_DSN,
-  })
-);
+app.use(clerkMiddleware()); 
 app.use(prettyJSON());
 app.use(cors());
 app.use(authMiddleware);
