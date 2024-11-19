f2ec946057ddace45317d24ca9d5b749fe2ff12d
= <anuragsawant@duck.com>
2024-11-19 12:54:31 +0530

    Refactor Vite configuration to load environment variables and standardize server port settings

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import { loadEnv } from "vite";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
+    server: {
+      port: parseInt(process.env.VITE_PORT!),
+    },
+
     plugins: [
       react(),
       visualizer({
         emitFile: true,
         filename: "stats.html",
-        template: "sunburst"
+        template: "sunburst",
       }),
     ],
     resolve: {
